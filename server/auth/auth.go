0b8b304e5aebd9b3853509a4936d0c176e8ff2a6
conditionals <iancrowley98@gmail.com>
2025-10-28 15:36:10 -0400

    update

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -21,4 +21,27 @@ func RegisterHandlers(router gin.IRouter, client *http.Client, log *zap.Logger) 
 	router.GET("/test", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"result": "ok"})
 	})
+
+	router.POST("/login", func(c *gin.Context) {
+		type LoginPayload struct {
+			User string `json:"user"`
+			Pass string `json:"pass"`
+		}
+
+		var payload LoginPayload
+		if err := c.BindJSON(&payload); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+			return
+		}
+
+		if payload.User == "notCreated" {
+			c.Status(http.StatusUnauthorized)
+			return
+		}
+
+		log.Info("Login request:", zap.String("user", payload.User))
+
+		c.Status(http.StatusOK)
+	})
+
 }
